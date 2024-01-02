1c96b6b6dc8f20d993f18b23156351e026075bf5
zhenyang1999 <71130436+zhenyang1999@users.noreply.github.com>
2024-01-03 00:06:53 +0800

    Add files via upload

---
+++ testing_db_connect.py
@@ -0,0 +1,29 @@
+import pymysql
+import sys
+import pandas as pd
+import streamlit as st
+
+# Connect to MariaDB
+try:
+    conn = pymysql.connect(
+        user=st.secrets.db_credentials.user,
+        password=st.secrets.db_credentials.password,
+        host=st.secrets.db_credentials.host,
+        port=st.secrets.db_credentials.port,
+        db=st.secrets.db_credentials.database
+
+    )
+except pymysql.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+#cur = conn.cursor()
+
+@st.cache_data
+def query():
+    return pd.read_sql( st.secrets.db_query.query_1 , con= conn)
+
+st.title('testing dataframe')
+df = query()
+st.dataframe(df.head(10))
